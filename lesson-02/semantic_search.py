44d6846c95a8df026b2638adba219084ef40d43f
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-06-20 07:06:37 +0200

    implemented vector search with filters

--- lesson-02/semantic_search.py
+++ lesson-02/semantic_search.py
@@ -2,6 +2,7 @@ from qdrant_client import QdrantClient, models
 from fastembed import TextEmbedding
 import requests
 import json
+import random
 
 client = QdrantClient("http://localhost:6333")
 
@@ -19,3 +20,62 @@ model_handle = "jinaai/jina-embeddings-v2-small-en"
 
 collection_name = "zoomcamp_rag"
 
+# running similarity search
+def search(query, limit=1):
+    results = client.query_points(
+        collection_name = collection_name,
+        query = models.Document( #embed the query text locally with "jinaai/jina-embeddings-v2-small-en"
+            text=query,
+            model=model_handle 
+        ),
+        limit = limit,
+        with_payload = True
+    )
+
+    return results
+
+# pick a random question from data
+course = random.choice(documents_raw)
+course_piece = random.choice(course['documents'])
+# print(json.dumps(course_piece, indent=2))
+
+# answer
+result = search(course_piece['question'])
+# 
+
+# print(f"Question:\n{course_piece['question']}\n")
+# print("Top Retrieved Answer:\n{}\n".format(result.points[0].payload['text']))
+# print("Original Answer:\n{}".format(course_piece['text']))
+
+# let’s search the answer to a question that wasn’t in the initial dataset.
+print(search("What if I submit homeworks late?").points[0].payload['text'])
+
+# turn on indexing of payload fields.
+client.create_payload_index(
+    collection_name = collection_name,
+    field_name = "course",
+    field_schema = "keyword"
+)
+
+def search_in_course(query, course="mlops-zoomcamp", limit=1):
+    results = client.query_points(
+        collection_name = collection_name,
+        query = models.Document( #embed the query text locally with "jinaai/jina-embeddings-v2-small-en"
+            text=query,
+            model=model_handle 
+        ),
+        query_filter = models.Filter(
+            must = [
+                models.FieldCondition(
+                    key = "course",
+                    match = models.MatchValue(value=course)
+                )
+            ]
+        ),
+        limit = limit,
+        with_payload = True
+    )
+
+    return results
+
+print(search_in_course("What if I submit homeworks late?", "mlops-zoomcamp").points[0].payload['text'])
